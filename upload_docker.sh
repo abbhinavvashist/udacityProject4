83d1917af134dbe1fb432cab540b1255fbe782e2
abhinav <abbhinav.vashishth@gmail.com>
2023-01-06 11:00:54 +0530

    abc

--- upload_docker.sh
+++ upload_docker.sh
@@ -9,7 +9,26 @@
 
 # Step 2:  
 # Authenticate & tag
+echo "Docker ID and Image: $dockerpath"#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath="abc/demolocal"
+
+# Step 2:  
+# Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag demolocal $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker image push $dockerpath
+
 
 # Step 3:
 # Push image to a docker repository
